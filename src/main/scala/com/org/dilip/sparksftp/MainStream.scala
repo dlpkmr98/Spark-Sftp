ca423d695994956d10df2da6bcd8b327549b2a32
Dilip Diwakar <dlpkmr98@gmail.com>
2018-05-24 19:26:54 +0200

    Update MainStream.scala

--- src/main/scala/com/org/dilip/sparksftp/MainStream.scala
+++ src/main/scala/com/org/dilip/sparksftp/MainStream.scala
@@ -8,37 +8,41 @@ import scala.util.Success
 import scala.util.Failure
 import com.org.dilip.pipeline.executor.PipelineExecutor
 import com.org.dilip.pipeline.executor.SyncExecution
+import scala.util.Try
 
 trait MainStreamService {
+  //required implicit declaration
   implicit def >>[A](c: A) = c.asInstanceOf[Pipeline[Seq[String], Some[String]]]
-  //use AsyncExecution to switch to parallel execution
-  implicit val pipelineExecutor: PipelineExecutor[Seq[String], Some[String]] = SyncExecution()
-  
-  def executePipline(pl: Pipeline[Seq[String], Some[String]]) = pl.start(Properties.input_path.split(",").toSeq) {
-    case Success(s)  => println(s"$s")
-    case Failure(ex) => ex.printStackTrace()
-  }
+  implicit def >>>[C](c: C) = c.toString()
 }
 
 class MainStream extends MainStreamService {
-  //way to add stages in pipeline
-  //change read/write format ex: MultiFileReader(Formatter.apply("parquet")), default format is csv
-  def createPipline =
-    Pipeline[Seq[String], Seq[String]]() >>
-      MultiFileReader(Formatter.apply()) >>
-      MultiFileWriter(Formatter.apply()) >>
-      SftpMultiFileWriter()
-  def init[A <: Pipeline[Seq[String], Some[String]]](f: => A) = executePipline(f)
-
+  def init[A, B <: Array[String], C <: String](f:(A,C) => Unit,f1: => A, f2: (B, C) => C) = (x: B) => (y: C) => f(f1,f2(x, y))
 }
 
 object MainStream {
   def apply() = new MainStream
-  val pipeline = MainStream() createPipline
-  def main(args: Array[String]): Unit = MainStream() init pipeline
+  //way to add stages in pipeline
+  //change read/write format ex: MultiFileReader(Formatter.apply("parquet")), default format is csv
+  def createPipline = Pipeline[Seq[String], Seq[String]]() >> MultiFileReader(Formatter.apply()) >>
+    MultiFileWriter(Formatter.apply()) >> SftpMultiFileWriter()
+    
+  def getInput(args: Array[String], in: String): String = {
+    Try(Option(args(0)).getOrElse(in)) match {
+      case Failure(ex) => in
+      case Success(s) => s.isEmpty() match {
+        case true  => in
+        case false => s
+      }
+    }
+  }
+  //use AsyncExecution to switch to parallel execution
+  implicit val pipelineExecutor: PipelineExecutor[Seq[String], Some[String]] = SyncExecution()
+  def executePipline(pl: Pipeline[Seq[String], Some[String]],in:String)= pl.start(in.split(",").toSeq) {
+    case Success(s)  => println(s"$s")
+    case Failure(ex) => ex.printStackTrace()
+  }
+  lazy val in = Properties.input_path
+  //functional way
+  def main(args: Array[String]): Unit = MainStream().init(executePipline,createPipline, getInput)(args)(in)
 }
-
-
-
-
-
