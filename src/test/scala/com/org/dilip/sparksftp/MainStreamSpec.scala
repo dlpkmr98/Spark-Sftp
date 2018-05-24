f082c9ba71e98d648239650d0043801d9d193185
Dilip Diwakar <dlpkmr98@gmail.com>
2018-05-24 19:32:17 +0200

    Update MainStreamSpec.scala

--- src/test/scala/com/org/dilip/sparksftp/MainStreamSpec.scala
+++ src/test/scala/com/org/dilip/sparksftp/MainStreamSpec.scala
@@ -3,11 +3,15 @@ package com.org.dilip.sparksftp
 import com.org.dilip.pipeline.pipeline.Pipeline
 import java.io.File
 import com.org.dilip.sparksftp.commons.Properties
+import com.org.dilip.sparksftp.jobs.MultiFileReader
+import com.org.dilip.sparksftp.jobs.Formatter
+import com.org.dilip.sparksftp.jobs.MultiFileWriter
+import com.org.dilip.sparksftp.jobs.SftpMultiFileWriter
 
 class MainStreamSpec extends UnitSpec with MiniSftpServer {
 
   "The Test" should "test spark-sftp project " in {
-    val res = MainStream() init MainStream.pipeline
+    val res = MainStream().init(MainStream.executePipline,MainStream.createPipline, MainStream.getInput)(Array(""))(Properties.input_path)
     res.isInstanceOf[Unit] shouldBe true
     implicit val fileName = Properties.file_name
     val file1 = new File("src/test/resources/sftp/" + Properties.currentTime + "0" + fileName)
