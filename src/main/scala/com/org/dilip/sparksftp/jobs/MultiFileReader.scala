541a8c4b11edb1e36d3bb48c427b30adf7053aa0
Dilip Diwakar <dlpkmr98@gmail.com>
2018-05-24 19:27:57 +0200

    Update MultiFileReader.scala

--- src/main/scala/com/org/dilip/sparksftp/jobs/MultiFileReader.scala
+++ src/main/scala/com/org/dilip/sparksftp/jobs/MultiFileReader.scala
@@ -3,18 +3,19 @@ package com.org.dilip.sparksftp.jobs
 import com.org.dilip.sparksftp.commons.IO
 import com.org.dilip.pipeline.pipeline.Transformation
 import org.apache.spark.sql.DataFrame
+import com.org.dilip.sparksftp.sparksftp._
 
 case class Formatter (format:String = "csv", sep:String =",",mode:String = "overwrite")
 
-class MultiFileReader[R<:Formatter](r: R) extends Transformation[Seq[String], Seq[DataFrame]] with IO {
+class MultiFileReader[R<:Formatter](r: R) extends Transformation[Seq[String], Seq[Option[RorD]]] with IO {
 
   /* (non-Javadoc)
  * @see com.org.dilip.pipeline.pipeline.Transformation#execute()
  */
   override def execute = x => for (path <- x) yield {
      r.format match {
-       case "csv" => toReadDataFromCsv(path)
-       case "parquet" => toReadDataFromParquet(path)
+       case "csv" => convert(toReadDataFromCsv(path))
+       case "parquet" => convert(toReadDataFromParquet(path))
        case _ => throw new RuntimeException("Only supported format are: csv,parquet")
      }
   }
